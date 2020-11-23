682edb104e3a4246a6328f2ce67eb0cd1b2ac4a0
masterkeze <861168745@qq.com>
2020-11-23 19:23:30 +0800

    init test project

--- regex/state.cs
+++ regex/state.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
 
-namespace regex {
+namespace FSM {
     class State {
         private Boolean _isEnd;
         private Dictionary<String, State> _transition;
         private List<State> _epsilonTransitions;
-        public void State (Boolean isEnd) {
+        public State (Boolean isEnd) {
             this._isEnd = isEnd;
             this._transition = new Dictionary<String, State> ();
             this._epsilonTransitions = new List<State> ();
@@ -16,6 +16,24 @@ namespace regex {
         }
         public void addEpsilonTransition (State to) {
             this._epsilonTransitions.Add(to);
+        }
+    }
+    class Basis {
+        public State startState;
+        public State endState;
+        public Basis(){
+            this.startState = new State(false);
+            this.endState = new State(true);
+        }
+    }
+    class SymbolBasis : Basis {
+        public SymbolBasis(String symbol) : base(){
+            this.startState.addTransition(this.endState,symbol);
+        }
+    }
+    class EpsilonBasis : Basis {
+        public EpsilonBasis() : base(){;
+            this.startState.addEpsilonTransition(this.endState);
         }
     }
 }
